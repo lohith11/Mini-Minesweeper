2fe312ec250a8e8cce7f8b076c9cb488b5201a23
harsh5424 <harshithtennyson10@gmail.com>
2022-07-31 15:58:22 +0530

    Started working on persistent menu settings

--- Assets/Scripts/DataCarrier.cs
+++ Assets/Scripts/DataCarrier.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class DataCarrier : MonoBehaviour
 {
-    public static string gameMode = "Custom Game"; // "Quick Game" or "Custom Game"
+    public static string gameMode = "Custom Game"; // "Quick Game" or "Custom Game" or "Zen Mode"
     public static int gridSize = 20; // 20 - 50
-    public static bool musicEnabled = true;
     public static string difficulty = "Medium"; // "Easy", "Medium", "Hard"
-
     public static string hearts = "3"; // "1", "3", "5", "-1"
 }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,50 +1,91 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+using Michsky.UI.ModernUIPack;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] Slider slider;
-    [SerializeField] GameObject menuCanvas, settingsCanvas;
+    [SerializeField] GameObject quickMask, customMask, zenMask;
 
     void Start()
     {
-        DataCarrier.difficulty = "Medium";
+        if (PlayerPrefs.GetInt("FirstLaunch") == 0)
+        {
+            PlayerPrefs.SetInt("FirstLaunch", 1);
+
+            PlayerPrefs.SetInt("Quick.GridSize", 20);
+            PlayerPrefs.SetString("Quick.Difficulty", "Medium");
+
+            PlayerPrefs.SetInt("Zen.GridSize", 50);
+            PlayerPrefs.SetString("Zen.Difficulty", "Medium");
+            PlayerPrefs.SetInt("Zen.Hearts", -1);
+
+            PlayerPrefs.SetInt("Custom.GridSize", 50);
+            PlayerPrefs.SetString("Custom.Difficulty", "Medium");
+            PlayerPrefs.SetInt("Custom.Hearts", 3);
+
+            PlayerPrefs.SetString("GameMode", "Quick");
+        }
+        else
+        {
+
+        }
     }
 
-    public void StartGame(string gameMode = "Custom Game")
+    public void GameModeSelected(string gameMode = "Quick")
     {
-        DataCarrier.gameMode = gameMode;
-        DataCarrier.gridSize = (int)slider.value;
-        SceneManager.LoadScene("Level");
+        PlayerPrefs.SetString("GameMode", gameMode);
+
+        if (gameMode == "Quick")
+        {
+            PlayerPrefs.SetInt("Quick.GridSize", Random.Range(20, 51));
+            quickMask.SetActive(true);
+
+            int index;
+            switch (PlayerPrefs.GetString("Quick.Difficulty"))
+            {
+                case "Easy": index = 0; break;
+                case "Medium": index = 1; break;
+                case "Hard": index = 2; break;
+                default: index = 1; break;
+            }
+            quickMask.transform.GetChild(0).GetComponent<TMP_Text>().text = "Grid Size: " + PlayerPrefs.GetInt("Quick.GridSize");
+            quickMask.transform.GetChild(1).transform.GetChild(0).GetComponent<HorizontalSelector>().index = index;
+
+            customMask.SetActive(false);
+            zenMask.SetActive(false);
+        }
+        else if (gameMode == "Custom")
+        {
+            customMask.SetActive(true);
+
+            int index;
+            switch (PlayerPrefs.GetString("Custom.Difficulty"))
+            {
+                case "Easy": index = 0; break;
+                case "Medium": index = 1; break;
+                case "Hard": index = 2; break;
+                default: index = 1; break;
+            }
+            quickMask.transform.GetChild(0).transform.GetChild(0).GetComponent<HorizontalSelector>().index = index;
+            //quickMask.transform.GetChild(0).GetComponent<TMP_Text>().text = "Grid Size: " + PlayerPrefs.GetInt("Quick.GridSize");
+
+            quickMask.SetActive(false);
+            zenMask.SetActive(false);
+        }
+        else if (gameMode == "Zen")
+        {
+            zenMask.SetActive(true);
+            quickMask.SetActive(false);
+            customMask.SetActive(false);
+        }
     }
 
-    public void OpenSettings()
+    public void DifficultyChanged(string difficulty = "Medium")
     {
-        menuCanvas.SetActive(false);
-        settingsCanvas.SetActive(true);
-    }
-
-    public void BackToMenu()
-    {
-        menuCanvas.SetActive(true);
-        settingsCanvas.SetActive(false);
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
-    public void DifficultyChanged(string difficulty)
-    {
-        DataCarrier.difficulty = difficulty;
-        Debug.Log("Difficulty: " + DataCarrier.difficulty);
-    }
-
-    public void HeartsChanged(string hearts)
-    {
-        DataCarrier.hearts = hearts;
-        Debug.Log("Hearts: " + DataCarrier.hearts);
+        string gameMode = PlayerPrefs.GetString("GameMode");
+        PlayerPrefs.SetString(gameMode + ".Difficulty", difficulty);
     }
 }
